7589c667deaf64c6264681974e7e4af24c215663
Ishola Sodiq Olatunde <ishola.olatundey@gmail.com>
2023-08-12 07:27:56 +0100

    First Commit

---
+++ src/components/index.jsx
@@ -0,0 +1,24 @@
+import Clients from './Clients'
+import CTA from './CTA'
+import Stats from './Stats'
+import Footer from './Footer'
+import Testimonials from './Testimonials'
+import Hero from './Hero'
+import NavBar from './NavBar'
+import CardDeal from './CardDeal'
+import Business from './Business'
+import Billing from './Billing'
+
+export{
+    // eslint-disable-next-line react-refresh/only-export-components
+    CTA,
+ NavBar,
+ Billing,
+ CardDeal,
+ Business,
+ Clients,
+ Stats,
+ Footer,
+ Testimonials,
+ Hero,
+}
